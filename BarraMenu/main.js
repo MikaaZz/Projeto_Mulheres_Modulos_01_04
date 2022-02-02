ae227654c622bb1148dea64c2a711d2e076bdd9c
Mikhael Zimmermann <mikhael928@gmail.com>
2022-02-02 17:16:37 -0300

    Criation Mod 01

--- BarraMenu/main.js
+++ BarraMenu/main.js
@@ -1,7 +1,6 @@
+/*script do menu*/
 const menuBtn = document.querySelector ('.navButton');
 let menuOpen = false;
-
-
 const showMenu = document.querySelector ('.navButtonClick')
 const displayFlex = "flex";
 const displayNone = "none";
@@ -16,10 +15,5 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.remove('open');
     menuOpen = false;
     showMenu.style.display = displayNone;
-    
  }
 });
-var elem = document.createElement('link');
-    elem.rel = 'stylesheet'
-    elem.href = 'https://escuela.cacmu.fin.ec/pluginfile.php/14692/course/section/1061/style.csss'; //Link do css
-    document.head.appendChild(elem);
